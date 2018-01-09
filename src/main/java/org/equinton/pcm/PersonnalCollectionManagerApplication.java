84de00498ecce3688c486df0a6ee039a4ba54ab8
Emmanuel <equinton.pro@gmail.com>
2018-01-09 11:15:22 +0100

    #4 Add stagemonitor's configuration

--- src/main/java/org/equinton/pcm/PersonnalCollectionManagerApplication.java
+++ src/main/java/org/equinton/pcm/PersonnalCollectionManagerApplication.java
@@ -2,11 +2,13 @@ package org.equinton.pcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.stagemonitor.core.Stagemonitor;
 
 @SpringBootApplication
 public class PersonnalCollectionManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonnalCollectionManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Stagemonitor.init();
+        SpringApplication.run(PersonnalCollectionManagerApplication.class, args);
+    }
 }
